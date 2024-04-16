5f8aa4cb42f715a04b9fec1b4a7fea7be9c81fc6
junik <wkdwnsdlr37@gmail.com>
2024-04-16 18:55:53 +0900

    Design: Header Comp leftPart

--- fsd/src/features/header/views/CenterPart.tsx
+++ fsd/src/features/header/views/CenterPart.tsx
@@ -3,7 +3,14 @@ import React from "react";
 const CenterPart = ()=>{
 
     return(
-        <div>가운데</div>
+        <div className="centerpart">
+            <div>
+                <input placeholder="검색"></input>
+                <img src="#" alt="키보드"/>
+                <button>검색 아이콘</button>
+            </div>
+            <div>마이크 아이콘</div>
+        </div>
     );
 }
 
