86451814f98df330f9ecc7f904f21bef7915ec10
junik <wkdwnsdlr37@gmail.com>
2024-04-17 20:01:42 +0900

    Refactor: member,guest data 적용

--- fsd/src/features/aside/Aside.tsx
+++ fsd/src/features/aside/Aside.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import "./aside.css";
 import IconBtn from "./views/IconBtn";
-import {data} from "./model/data";
+import {memberData,guestData} from "./model/data";
+import useCheckUser from "@hooks/useCheckUser";
+
 
 const Aside = ()=>{
+    const authorization = useCheckUser();
 
     return(
         <div className="aside">
-            {data && data.map((value,idx)=>{
+            {authorization 
+            ? (memberData && memberData.map((value,idx)=>{
                 return <IconBtn key={idx} data={value}/>
-            })}
+            })) 
+            : (guestData && guestData.map((value,idx)=>{
+                return <IconBtn key={idx} data={value}/>
+            }))}
         </div>
     );
 }
--- fsd/src/features/aside/model/data.ts
+++ fsd/src/features/aside/model/data.ts
@@ -2,9 +2,11 @@ import home from "@image/home.svg";
 import shorts from "@image/shorts.svg";
 import subscriptions from "@image/subscriptions.svg";
 import my from "@image/my.svg";
+import time from "@image/time.svg";
+import music from "@image/music.svg";
 
 
-export const data = [
+export const memberData = [
     {
         file: home,
         text: "홈",
@@ -23,6 +25,38 @@ export const data = [
     {
         file: my,
         text: "나",
+        router: "./",
+    },
+    {
+        file: music,
+        text: "음악",
+        router: "./",
+    },
+]
+export const guestData = [
+    {
+        file: home,
+        text: "홈",
+        router: "./",
+    },
+    {
+        file: shorts,
+        text: "shorts",
+        router: "./",
+    },
+    {
+        file: subscriptions,
+        text: "구독",
+        router: "./",
+    },
+    {
+        file: my,
+        text: "나",
+        router: "./",
+    },
+    {
+        file: time,
+        text: "시청기록",
         router: "./",
     },
 ]
